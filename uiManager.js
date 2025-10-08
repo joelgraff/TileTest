a63d0e406419f3a494a12f5859b2dbc4e1532052
Joel Graff <monograff76@gmail.com>
2025-10-08 08:29:30 -0500

    version update

--- uiManager.js
+++ uiManager.js
@@ -222,7 +222,7 @@ class UIManager {
     }
 
     createVersionDisplay() {
-        this.versionText = this.scene.add.text(10, 620, 'Version 0.9', {
+        this.versionText = this.scene.add.text(10, 620, 'Version 1.0', {
             fontFamily: 'Courier New, monospace',
             fontSize: '12px',
             fill: '#FFFFFF',
